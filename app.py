5e5715ad7dbf3e9113fdaf0d968ad67887d113fa
Kohki YAMAGIWA <tamanobi@gmail.com>
2017-07-07 00:04:40 +0900

    wip

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import os
+import io
 from flask import Flask, url_for, request, jsonify
 from werkzeug.utils import secure_filename
 from PIL import Image
@@ -11,8 +12,6 @@ app = Flask(__name__)
 with open('i2v.pid','w') as f:
     pid = os.getpid()
     f.write(str(pid)+"\n")
-    with open('/proc/' + str(pid) + '/oom_adj', 'w') as f2:
-        f2.write("-17\n")
 
 illust2vec = i2v.make_i2v_with_chainer("illust2vec_tag_ver200.caffemodel", "tag_list.json")
 
@@ -30,9 +29,8 @@ def i2v_feature2():
     f = request.files['the_file']
     #fname = './static/images/'+secure_filename(f.filename)
     #f.save(fname)
-    with f.read() as img:
+    with Image.open(f.stream.read()) as img:
         feature = illust2vec.extract_feature([img])[0]
-        os.remove(fname)
         j = jsonify({'feature': feature.tolist()})
     return j
 
